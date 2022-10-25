c252b2ea552437aefa221aa874e58cab48883585
n-s-j <46404544+n-s-j@users.noreply.github.com>
2022-10-25 16:21:58 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@
 	
 	<div class="overlay top right flex-col gap">
 		<div v-if="!state.started" class="settings">
-			<h4 class="center mb-2">Settings ({{ collapse_content() }})</h4>
+			<h4 class="center mb-2">Settings {{ collapse_content() }}</h4>
 			<div class="content_test">
 				<h4 class="center">testest</h4>
 			</div>
